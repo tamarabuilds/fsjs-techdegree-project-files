d1202b765d3446c1c6b15dd3f395e0ab71a43ff3
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-11 15:43:59 -0700

    initial commit

---
+++ structuring-javascript-code/2 - Iterative Coding/iterative-coding_final/iterative_coding_complete.js
@@ -0,0 +1,22 @@
+const word = "computer";
+const index = Math.floor(Math.random() * word.length);
+const letter = "b";
+
+/**
+  * Replaces a letter in a word
+  * @param {string} string - Word being changed
+  * @param {number} index - Index of letter to be replaced
+  * @param {string} letter - letter to replace above letter with
+  * @return {string} Word after letter has been replaced
+  */
+function replaceLetter(string, index, letter){
+  const splitString = string.split("");
+  splitString[index] = letter;
+  const newString = splitString.join("");
+
+  console.log(newString);
+  
+  return newString;
+}
+
+replaceLetter(word, index, letter);
