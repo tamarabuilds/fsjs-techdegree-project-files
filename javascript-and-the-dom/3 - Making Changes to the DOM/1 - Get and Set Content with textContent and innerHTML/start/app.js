ae20c7708f25b5d6488d3e23b0d04a874eb6cc6b
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-12 15:56:58 -0700

    lessons on setting inline styles with style property

--- javascript-and-the-dom/3 - Making Changes to the DOM/1 - Get and Set Content with textContent and innerHTML/start/app.js
+++ javascript-and-the-dom/3 - Making Changes to the DOM/1 - Get and Set Content with textContent and innerHTML/start/app.js
@@ -1,17 +1,39 @@
 const btnUpdate = document.querySelector('.btn-main');
-const headline = document.getElementById('headline');
-const items = document.querySelectorAll('li');
-const highlights = document.querySelectorAll('.highlight');
+const btnToggle = document.querySelector('.btn-toggle')
 
-for ( const highlight of highlights ) {
-  highlight.style.backgroundColor = 'cornsilk';
-}
-
-for (let i = 0; i < items.length; i++) {
-  items[i].style.color = 'orchid';
-}
-
+ 
 btnUpdate.addEventListener('click', () => {
-  headline.style.border = 'solid 2px red';
-  headline.style.fontSize = '60px';													 
+  const headline = document.getElementById('headline');
+  const input = document.querySelector('.input-main')
+  headline.className = 'grow'
+
+  headline.textContent = input.value
+  input.value = ''
 });
+
+btnToggle.addEventListener('click', () => {
+const listContainer = document.querySelector('.list-container')
+
+if ( listContainer.style.display === 'none') {
+  btnToggle.textContent = 'hide'
+  listContainer.style.display = 'block'   // Fails to update style to block
+  // listContainer.removeAttribute('style')     // This works
+} else {
+  listContainer.style.display = 'none'
+  btnToggle.textContent = 'show'
+}
+
+})
+
+
+// btnToggle.addEventListener('click', () => {
+//   const listContainer = document.querySelector('.list-container');
+
+// 	if (listContainer.style.display === 'none') {
+// 		btnToggle.textContent = 'Hide List';
+// 		listContainer.removeAttribute('style');
+// 	} else {
+// 		btnToggle.textContent = 'Show List';
+// 		listContainer.style.display = 'none'; 	
+// 	}		
+// });
