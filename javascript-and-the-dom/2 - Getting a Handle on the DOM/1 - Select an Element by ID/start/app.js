f47b3273c4ce95e6ff7ac276266f87e5d229415c
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-12 10:48:32 -0700

    worked through select elements by tag name

--- javascript-and-the-dom/2 - Getting a Handle on the DOM/1 - Select an Element by ID/start/app.js
+++ javascript-and-the-dom/2 - Getting a Handle on the DOM/1 - Select an Element by ID/start/app.js
@@ -1,10 +1,17 @@
 const headline = document.getElementById('headline')
-const main = document.getElementById('btn-main')
+const btnUpdate = document.getElementById('btn-main')
 
+const items = document.getElementsByTagName('li')
 
-headline.style.border = `solid 2px red`
+for (let i = 0; i < items.length; i++){
+  items[i].style.color = 'orchid'
 
-main.addEventListener('click', () => {
-  main.style.border = 'solid 2px yellow'
+  console.log(items[i])
+}
+
+btnUpdate.addEventListener('click', () => {
+  // main.style.border = 'solid 2px yellow'
+  headline.style.border = `solid 2px red`
+  headline.style.fontSize = `60px`
 
 })
