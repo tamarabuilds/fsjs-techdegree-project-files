a753669fe6c6f0d7e84129f3923394e14bde7006
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-13 16:02:14 -0700

    lesson on rsvp checkbox

--- 1-adding-and-removing-names/1-tour-the-application/start/app.js
+++ 1-adding-and-removing-names/1-tour-the-application/start/app.js
@@ -1,13 +1,31 @@
 const form = document.getElementById('registrar')
 const input = form.querySelector('input')
+const ul = document.getElementById('invitedList')
 
 form.addEventListener('submit', (event)=> {
     event.preventDefault()
     const text = input.value
     input.value = ''
     const li = document.createElement('li')
-    const ul = document.getElementById('invitedList')
     li.textContent = text
+    const label = document.createElement('label')
+    label.textContent = 'Confirmed'
+    const checkbox = document.createElement('input')
+    checkbox.type = 'checkbox'
+    label.appendChild(checkbox)
+    li.appendChild(label)
     ul.appendChild(li)
     
+})
+
+ul.addEventListener('change', (event)=> {
+    const checkbox = event.target
+    const checked = checkbox.checked
+    const listItem = checkbox.parentNode.parentNode
+    
+    if ( checked ) {
+        listItem.className = 'responded'
+    } else {
+        listItem.className = ''
+    }
 })
