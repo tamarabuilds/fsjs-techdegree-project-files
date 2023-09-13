8be495aa103ac0dac06692f196602a1858408310
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-13 11:59:58 -0700

    initial adding and removing names

--- Interacting with the DOM/2 - Traversing the DOM/2 - Event Bubbling and Delegation/app.js
+++ Interacting with the DOM/2 - Traversing the DOM/2 - Event Bubbling and Delegation/app.js
@@ -3,16 +3,45 @@ const btnCreate = document.getElementById("btn-main");
 const btnToggle = document.querySelector(".btn-toggle");
 const btnRemove = document.querySelector(".btn-remove");
 const taskList = document.querySelector(".list-container ul");
+const listItems = taskList.children
+
+function attachRemoveButton(li){
+	let remove = document.createElement('button')
+	remove.className = 'remove'
+	remove.textContent = 'Remove'
+	li.appendChild(remove)
+}
+
+for  (let i = 0; i < listItems.length; i++){
+	attachRemoveButton(listItems[i])
+}
 
 
+btnCreate.addEventListener('click', ()=> {
+	let ul = document.getElementsByTagName('ul')[0]
+	const input = document.querySelector('.input-main')
+	let li = document.createElement('li')
+	li.textContent = input.value
+	ul.appendChild(li)
+	attachRemoveButton(li)
+	input.value = ''
+})
 
-taskList.addEventListener("mouseover", (event) => {
-	if (event.target.tagName === 'LI'){
-		event.target.textContent = event.target.textContent.toUpperCase()
+
+taskList.addEventListener("click", (event) => {
+	if (event.target.tagName === 'BUTTON'){
+		const button = event.target
+		const li = button.parentNode
+		li.remove()
 	}
-	// console.log(event)
-
 });
+
+
+// taskList.addEventListener("mouseover", (event) => {
+// 	if (event.target.tagName === 'BUTTON'){
+		
+// 	}
+// });
 
 btnToggle.addEventListener("click", () => {
 	const listContainer = document.querySelector(".list-container");
@@ -25,15 +54,11 @@ btnToggle.addEventListener("click", () => {
 	}
 });
 
-btnCreate.addEventListener("click", () => {
-	const input = document.querySelector(".input-main");
-	const list = document.querySelector("ul");
-	list.insertAdjacentHTML("afterbegin", `<li>${input.value}</i>`);
-	input.value = "";
-});
+// btnCreate.addEventListener("click", () => {
+// 	const input = document.querySelector(".input-main");
+// 	const list = document.querySelector("ul");
+// 	list.insertAdjacentHTML("afterbegin", `<li>${input.value}</i>`);
+// 	input.value = "";
+// });
 
 
-btnRemove.addEventListener("click", () => {
-	const li = document.querySelector("li:last-child");
-	li.remove();
-});
