86f2936198250d05854c5313dfb549e0ac83d5da
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-09-14 16:36:12 -0700

    draft of pagination practice

---
+++ practice-data-pagination/js/data.js
@@ -0,0 +1,57 @@
+const authors = [
+  {
+    name: "James Kirby",
+    image: "./assets/authors/a0.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque tempora eius consequatur ea, repellat quos.",
+  },
+  {
+    name: "George Renolds",
+    image: "./assets/authors/a1.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio placeat omnis recusandae doloribus dolorem harum delectus, incidunt laborum. Natus dolorum reprehenderit repellendus aperiam voluptates nostrum.",
+  },
+  {
+    name: "Leslie Jacobs",
+    image: "./assets/authors/a2.png",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laboriosam quam inventore iusto aliquid rerum similique excepturi, animi quidem, dolorum nulla minus accusantium eveniet, dignissimos expedita ipsum a? Veniam quaerat, tempora rerum, sit unde nulla magnam provident velit impedit quis esse!",
+  },
+  {
+    name: "Salem Johnson",
+    image: "./assets/authors/a3.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque tempora eius consequatur ea, repellat quos.",
+  },
+  {
+    name: "Lenny Burk",
+    image: "./assets/authors/a4.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ea nulla eum nam minima dignissimos, beatae illo, provident voluptates deleniti aut voluptatibus quidem neque aliquam magnam? Hic, molestiae ducimus!",
+  },
+  {
+    name: "Stephanie June",
+    image: "./assets/authors/a5.png",
+    text: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.Consectetur sit commodi facilis voluptatibus totam ipsum minima tempora, a enim. Quidem, quas modi.",
+  },
+  {
+    name: "Bryce Cason",
+    image: "./assets/authors/a6.png",
+    text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam cum ipsum, voluptas reiciendis, adipisci quo et repellat ullam blanditiis dolores illo!",
+  },
+  {
+    name: "Trey Henting",
+    image: "./assets/authors/a7.png",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.Voluptatem amet ea tempora, labore voluptates minima incidunt ipsa quisquam, obcaecati tempore sequi asperiores reiciendis quas aspernatur atque omnis magni repudiandae dolorum hic nesciunt deserunt quidem libero velit! Cum, voluptate. Ut, ab excepturi recusandae sit laudantium molestias earum ratione rerum est repellat.",
+  },
+  {
+    name: "Julius Markers",
+    image: "./assets/authors/a8.png",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi libero aspernatur quis optio amet repellat!",
+  },
+  {
+    name: "Susan Goodman",
+    image: "./assets/authors/a9.png",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate consequuntur tenetur maxime fuga quae fugiat in nihil repellendus ratione, hic esse, aliquid et totam a, itaque earum? Placeat ullam nihil iusto, voluptate mollitia magni fuga provident esse quo deserunt!",
+  },
+  {
+    name: "Samantha Ashton",
+    image: "./assets/authors/a10.png",
+    text: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Modi, facilis placeat omnis illo ipsam asperiores neque beatae deleniti sequi, magni, suscipit dolores dolor harum velit.",
+  },
+];
---
+++ practice-data-pagination/js/script.js
@@ -0,0 +1,103 @@
+// ELEMENT SELECTORS
+const authorContainer = document.querySelector("#authorContainer");
+const paginationList = document.querySelector("#paginationList");
+
+// console.log(authors);
+const authorsPerPage = 3;
+
+/* This function will handle calculating how many buttons are
+needed and dynamically add them to the page */
+
+function handlePagination(array) {
+  // 1. Create a variable to store the number of buttons needed.
+  //    The math should be (the length of the array divided by the authorsPerPage) rounded up
+  //    Hint: Math.ceil()
+  const pages = Math.ceil(array.length / authorsPerPage)
+  // 2-a. Start a loop to the length of the number of buttons calculated above.
+  for ( let i = 1; i <= pages; i++){
+    // 2-b. Inside, create a variable storing a template literal of the HTML markup of a button
+    //      (see example in index.html lines 34 - 36).
+    let html = `<li>
+                    <button type='button' name='button'>${i}</button>
+                </li>`
+    // 2-c. Then add this variable to the paginationList element
+    //      Hint: insertAdjacentHTML()
+    paginationList.insertAdjacentHTML('beforeend', html)
+  }
+  // 3. Add the `active` class to the first button
+  //    Hint: querySelector()
+  const firstPageNumber = paginationList.querySelector('li')            // is there a better way to select?
+  firstPageNumber.className = 'active'
+  // console.log(paginationList)
+  // console.log(firstPageNumber)
+  
+}
+
+/* This function will handle calculating how many and which
+authors to show on the current page and dynamically add them */
+
+function showPage(array, page) {
+  // 4. Create a variable to represent which author to start with on the page.
+  //    The math should be (the page multiplied by the authorsPerPage) minus the authorsPerPage
+  const authorStart = (page * authorsPerPage) - authorsPerPage 
+  // 5. Create a variable to represent which author to end with on the page.
+  //    The math should be (the page multiplied by the authorsPerPage) minus one
+  const authorEnd = (page * authorsPerPage) - 1     // minus 1 for starting at zero index
+  // 6. Reset the authorContainer's content to nothing to prevent previous cards staying on the page            //!!!!!!?!?!?!
+
+  // while ()
+
+
+  // 7-a. Start a loop to the length of the array's length
+  for (let i = 0; i < array.length; i++){
+    // 7-b. Inside, create a conditional checking if `i` is...
+    //      - greater than or equal to the start variable
+    //      - less than or equal to the end variable
+    if ( i >= authorStart && i <= authorEnd){
+      // 7-c. If true, create a variable storing a template literal of the HTML markup of an author card.
+      //      (see example in index.html lines 17 - 28).
+      //      Hint: You'll need to dynamically add each author's information
+      // 7-d. Then add this variable to the authorContainer element
+      //      Hint: insertAdjacentHTML()
+      let html = `<div class="author-card">
+                    <div class="card-header">
+                      <img src=${array[i]['image']} />
+                    </div>
+                    <div class="card-content">
+                      <h2 class="title">${array[i]['name']}</h2>
+                      <p>
+                        ${array[i]['text']}
+                      </p>
+                    </div>
+                  </div> `
+      authorContainer.insertAdjacentHTML('beforeend', html)
+    }
+  }
+}
+
+/* This event listener will handle calling our function
+above to change the page & add the `active` class  */
+
+paginationList.addEventListener("click", (e) => {
+  // 8. Create a variable to store the button which currently has the `active` class
+  // console.log(e)
+  let activeButton = document.querySelector('.active')
+  const clickedButton = e.target
+  // 9-a. Make sure the user has clicked a `button`
+  //      Hint: e.target
+  if ( clickedButton.type === 'button'){
+    // 9-b. If true...
+    //      - Remove the `active` class from the currently active button
+    //      - Add the `active` class to the button just clicked
+    //      - Call showPage() passing it `authors` and the content of the button just clicked.
+
+    activeButton.className = ''
+    clickedButton.className = 'active'
+    showPage(authors, activeButton.textContent)
+  }
+});
+
+/* These function calls are needed to initialize the page */
+
+handlePagination(authors);
+showPage(authors, 1);
